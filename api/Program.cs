0e0e518a7f819cdf4365878c19edaa3c25b3ad8d
kubraacelik <kkbra.celik92@gmail.com>
2024-08-22 21:20:05 +0300

    defined IRepository

--- api/Program.cs
+++ api/Program.cs
@@ -1,4 +1,6 @@
 using api.Data;
+using api.Interfaces;
+using api.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +15,9 @@ builder.Services.AddDbContext<ApplicationDBContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+//? Bir sınıf IStockRepository isterse, ona StockRepository sınıfını ver. Bunu her istek (HTTP request) için bir kez yap.
+builder.Services.AddScoped<IStockRepository, StockRepository>();
 
 var app = builder.Build();
 
