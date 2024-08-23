b2bece17c3d27e001639d220d8e8ffa9b71a3e8e
kubraacelik <kkbra.celik92@gmail.com>
2024-08-23 13:11:47 +0300

    added NewtonSoft

--- api/Program.cs
+++ api/Program.cs
@@ -10,6 +10,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//? bir ASP.NET Core uygulamasında JSON serileştirme işlemlerini yapılandırmak için kullanılır.
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+{
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+});
+
 //? Context tanımlandı
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
 {
@@ -18,6 +24,7 @@ builder.Services.AddDbContext<ApplicationDBContext>(options =>
 
 //? Bir sınıf IStockRepository isterse, ona StockRepository sınıfını ver. Bunu her istek (HTTP request) için bir kez yap.
 builder.Services.AddScoped<IStockRepository, StockRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 
 var app = builder.Build();
 
