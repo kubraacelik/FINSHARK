d73b93fbc32c0c33d05dbdf1f4af0f43e9a9ddcc
kubraacelik <kkbra.celik92@gmail.com>
2024-08-24 14:03:51 +0300

    updated API

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf7144c6a907ee6c9eaa340d16851943cbbeb125")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c95a81d1ce0d788c2593b2e8cbc73b3c783d9ff1")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
