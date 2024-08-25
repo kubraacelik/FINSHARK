fc8d211c20e662db48f4270bdcc1ec1ca7ec7e5b
kubraacelik <kkbra.celik92@gmail.com>
2024-08-25 15:23:43 +0300

    Added user authentication and authorization

--- api/Program.cs
+++ api/Program.cs
@@ -1,6 +1,8 @@
 using api.Data;
 using api.Interfaces;
+using api.Models;
 using api.Repository;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,6 +23,17 @@ builder.Services.AddDbContext<ApplicationDBContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+//? Kullanıcı kimlik doğrulama ve yetkilendirme için kullanılır
+builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequiredLength = 12;
+})
+.AddEntityFrameworkStores<ApplicationDBContext>();
 
 //? Bir sınıf IStockRepository isterse, ona StockRepository sınıfını ver. Bunu her istek (HTTP request) için bir kez yap.
 builder.Services.AddScoped<IStockRepository, StockRepository>();
