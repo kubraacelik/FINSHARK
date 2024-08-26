a45ae2308da521b9e5ac1e966b5bd7098290ba2d
kubraacelik <kkbra.celik92@gmail.com>
2024-08-26 16:04:27 +0300

    Route definitions were made for HTTP requests.

--- api/Program.cs
+++ api/Program.cs
@@ -2,29 +2,31 @@ using api.Data;
 using api.Interfaces;
 using api.Models;
 using api.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//? Controller tanımlandı
+//* Controller tanımlandı
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//? bir ASP.NET Core uygulamasında JSON serileştirme işlemlerini yapılandırmak için kullanılır.
+//* bir ASP.NET Core uygulamasında JSON serileştirme işlemlerini yapılandırmak için kullanılır.
 builder.Services.AddControllers().AddNewtonsoftJson(options =>
 {
     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
 });
 
-//? Context tanımlandı
+//* Context tanımlandı
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-//? Kullanıcı kimlik doğrulama ve yetkilendirme için kullanılır
+//* Kullanıcı kimlik doğrulama ve yetkilendirme için kullanılır
 builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
 {
     options.Password.RequireDigit = true;
@@ -35,7 +37,31 @@ builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
 })
 .AddEntityFrameworkStores<ApplicationDBContext>();
 
-//? Bir sınıf IStockRepository isterse, ona StockRepository sınıfını ver. Bunu her istek (HTTP request) için bir kez yap.
+//* JSON Web Token (JWT) kimlik doğrulamasını yapılandırmak için kullanılır.
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme =
+    options.DefaultChallengeScheme =
+    options.DefaultForbidScheme =
+    options.DefaultScheme =
+    options.DefaultSignInScheme =
+    options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = builder.Configuration["JWT:Issuer"],
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["JWT:Audience"],
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(
+            System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigningKey"])
+        )
+    };
+});
+
+//* Bir sınıf IStockRepository isterse, ona StockRepository sınıfını ver. Bunu her istek (HTTP request) için bir kez yap.
 builder.Services.AddScoped<IStockRepository, StockRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 
@@ -49,7 +75,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-//? Uygulamanın HTTP isteklerini karşılamak için kullanılan routes tanımlar.
+//* Kullanıcının kimliğini doğrular.
+app.UseAuthentication();
+
+//*  Kimliği doğrulanmış kullanıcının belirli bir kaynağa erişim yetkisine sahip olup olmadığını kontrol eder.
+app.UseAuthorization();
+
+//* Uygulamanın HTTP isteklerini karşılamak için kullanılan routes tanımlar.
 app.MapControllers();
 
 app.Run();
